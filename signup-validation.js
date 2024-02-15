39bb2ee71822e20b5f1c0ccd0993718aeac2479a
albarr15 <cmalbarracin15@gmail.com>
2024-02-16 04:27:25 +0800

    Signup validation

---
+++ signup-validation.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var pw1 = document.getElementById("pwd1");
+    var pw2 = document.getElementById("pwd2");
+    var signupBtn = document.getElementById("confirm-signup-btn");
+
+    signupBtn.addEventListener("click", function() {
+
+        if (pw1.value === pw2.value) {
+            window.location.href = "mainpage.html";
+        } else {
+            pw1.style.backgroundColor = "red";
+            pw2.style.backgroundColor = "red";
+        }
+    });
+});
