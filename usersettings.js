0f558189882aed93528a237e6a21b8e3d554270c
gitnadu <121154953+gitnadu@users.noreply.github.com>
2024-02-15 23:07:48 +0800

    Add files via upload

---
+++ usersettings.js
@@ -0,0 +1,49 @@
+function openChangeName() {
+
+    var toggle = document.getElementById("settings-changingname");
+
+    if (toggle) {
+        toggle.classList.toggle("settings-changingname");
+    }
+
+}
+
+function openChangeUserName() {
+
+    var toggle = document.getElementById("settings-changingusername");
+
+    if (toggle) {
+        toggle.classList.toggle("settings-changingusername");
+    }
+
+}
+
+function openChangePass() {
+
+    var toggle = document.getElementById("settings-changingpass");
+
+    if (toggle) {
+        toggle.classList.toggle("settings-changingpass");
+    }
+
+}
+
+function openChangePfp() {
+
+    var toggle = document.getElementById("settings-changingprofilepic");
+
+    if (toggle) {
+        toggle.classList.toggle("settings-changingprofilepic");
+    }
+
+}
+
+function openChangeDescription() {
+
+    var toggle = document.getElementById("settings-changingdesc");
+
+    if (toggle) {
+        toggle.classList.toggle("settings-changingdesc");
+    }
+
+}
