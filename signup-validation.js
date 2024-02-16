a45db332f09d6e1f98e34b34f892943c3142c9e0
gitnadu <121154953+gitnadu@users.noreply.github.com>
2024-02-16 12:16:41 +0800

    Add files via upload

--- signup-validation.js
+++ signup-validation.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function() {
     signupBtn.addEventListener("click", function() {
 
         if (pw1.value === pw2.value) {
-            window.location.href = "mainpage.html";
+            window.location.href = "tftplayer100-mainpage.html";
         } else {
             pw1.style.backgroundColor = "red";
             pw2.style.backgroundColor = "red";
