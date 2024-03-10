a20bb58a7849c1912b5108a6bb4c47e6f268f18d
gitnadu <121154953+gitnadu@users.noreply.github.com>
2024-03-10 18:36:14 +0800

    adding mainpage, hbs

--- index.js
+++ index.js
@@ -12,10 +12,74 @@ const path = require('path')
 
 
 app.use(express.json()) // use json
+app.use(express.urlencoded( {extended: true})); // files consist of more than strings
 app.use(express.static('public')) // we'll add a static directory named "public"
 
 var hbs = require('hbs')
 app.set('view engine','hbs');
+
+const date = new Date();
+/*
+Post.create({
+    postId: 100,
+    title: 'Doggy2',
+    description: 'This is my dog2',
+    comments:[],
+    image: '/images/dog2.jpg',
+    tag: 'nada2',
+    upvotes: [],
+    downvotes: [],
+    popVal: 0,
+    trendVal: 0,
+    controVal: 0,
+    date: date,
+});
+*/
+/*
+Comment.create({
+    commentId: 12345,
+    userId: 54321,
+    theComment: "Hello World",
+    upvotes: [1,2,3,4],
+    downvotes: [4321],
+    date: date,
+    popVal: 45
+});
+*/
+/*s
+Post.create({
+    postId: 101,
+    title: 'Doggy',
+    description: 'This is my dog',
+    comments:[],
+    image: '/images/dog1.jpg',
+    tag: 'nada',
+    upvotes: [],
+    downvotes: [],
+    popVal: 0,
+    trendVal: 0,
+    controVal: 0,
+    date: date,
+});
+
+*/
+
+
+/*
+Comment.create({
+    commentId: 12345,
+    userId: 54321,
+    theComment: "Hello World",
+    upvotes: [1,2,3,4],
+    downvotes: [4321],
+    date: date,
+    popVal: 45
+});
+*/
+
+
+
+
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '\\' + 'index.html');
@@ -27,6 +91,13 @@ app.get('/login-page.html', function(req, res){
 
 app.get('/signup-page.html', function(req, res){
     res.sendFile(__dirname + '\\' + 'signup-page.html');
+});
+
+app.get('/mainpage', async(req, res) => {
+    const allPosts = await Post.find({})
+    console.log(allPosts);
+    res.render('mainpage',{allPosts})
+    
 });
 
 var server = app.listen(3000, function () {
