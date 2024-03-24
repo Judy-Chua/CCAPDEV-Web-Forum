468f4338bb15f99c8e4eab97ad8d372fb91433a9
albarr15 <cmalbarracin15@gmail.com>
2024-03-24 19:52:18 +0800

    Added initial code for signup validation

---
+++ controllers/userController.js
@@ -0,0 +1,40 @@
+const userModel = require('../models/User');
+const { validationResult } = require('express-validator');
+
+exports.signupUser = (req, res) => {
+
+    const errors = validationResult(req);
+
+    if (errors.isEmpty()) {
+        const {name, email, pwd1 } = req.body;
+        
+        // Next items go here...
+        // This line can be deleted in the next step
+        // Adding it so that the error validation can be tested.
+        res.redirect('/login');
+    } else {
+        const messages = errors.array().map((item) => item.msg);
+
+        req.flash('error_msg', messages.join(' '));
+        res.redirect('/signup');
+    }
+    res.redirect('/login');
+};
+
+exports.loginUser = (req, res) => {
+
+
+
+
+
+    res.redirect('/');
+};
+
+exports.logoutUser = (req, res) => {
+
+
+
+
+    res.redirect('/login');
+};
+
---
+++ routes/auth.js
@@ -0,0 +1,27 @@
+const router = require('express').Router();
+const userController = require('../controllers/userController');
+const { registerValidation } = require('../views/validators.js');
+
+// GET login to display login page
+router.get('/login', (req, res) => {
+    res.render('login-page', {
+        pageTitle: 'Login'
+    });
+});
+
+// GET signup to display signup page
+router.get('/signup', (req, res) => {
+    res.render('signup-page', {
+        pageTitle: 'Sign Up'
+    });
+});
+
+
+// POST methods for form submissions
+router.post('/signup', signupValidation, userController.signupUser);
+router.post('/login', userController.loginUser);
+
+// logout
+router.get('/logout', userController.logoutUser);
+
+module.exports = router;
