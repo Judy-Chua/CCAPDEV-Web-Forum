c6a04018af9a766158124e3e62b42a46715b33a7
albarr15 <cmalbarracin15@gmail.com>
2024-03-24 21:17:24 +0800

    Adding updated index.js

--- index.js
+++ index.js
@@ -769,35 +769,6 @@ app.post('/comments/:postId', async (req, res) => {
 });
 
 
-const router = require('express').Router();
-const userController = require('./controllers/userController.js');
-const { signupValidation } = require('./views/validators.js');
-
-// GET login to display login page
-router.get('/login', (req, res) => {
-    res.render('login-page', {
-        pageTitle: 'Login'
-    });
-});
-
-// GET signup to display signup page
-router.get('/signup', (req, res) => {
-    res.render('signup-page', {
-        pageTitle: 'Sign Up'
-    });
-});
-
-
-// POST methods for form submissions
-router.post('/signup', signupValidation, userController.signupUser);
-router.post('/login', userController.loginUser);
-
-// logout
-router.get('/logout', userController.logoutUser);
-
-module.exports = router;
-
-
 
 var server = app.listen(3000, function () {
     console.log('Node server is running..');
